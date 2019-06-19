9cedfb9dc6a1f6f24521d0abe1e5e354d6a74e8c
pkpkpkpkpkpkpkpk <pk.2121@gmail.com>
2019-06-19 16:39:16 +1000

    update port for deployment

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const cors_proxy = require('cors-anywhere');
 
-var host = 'https://pkpkpkpkpkpkpkpk.github.io/cors-anywhere-server';
-var port = 8080;
-
 cors_proxy
   .createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
   })
-  .listen(port, host, () => (
-    console.log(`CORS Anywhere server running on ${port}`)
+  .listen(process.env.PORT || 8080, () => (
+    console.log(`CORS Anywhere server running on ${host}:${port}`)
   ));
